18e272fab405d8c7feff1e52be6fbc300367992c
Edward Kuan <edward880814@gmail.com>
2023-08-14 09:48:16 +0800

    feat: update post controller unit test

--- src/features/post/routes/postRoutes.ts
+++ src/features/post/routes/postRoutes.ts
@@ -19,7 +19,7 @@ class PostRoutes {
     this.router.post('/post', authMiddleware.checkAuthentication, Create.prototype.post);
     this.router.post('/post/image/post', authMiddleware.checkAuthentication, Create.prototype.postWithImage);
 
-    this.router.put('/post/:postId', authMiddleware.checkAuthentication, Update.prototype.post);
+    this.router.put('/post/:postId', authMiddleware.checkAuthentication, Update.prototype.posts);
     this.router.put('/post/image/:postId', authMiddleware.checkAuthentication, Update.prototype.postWithImage);
 
     this.router.delete('/post/:postId', authMiddleware.checkAuthentication, Delete.prototype.post);
