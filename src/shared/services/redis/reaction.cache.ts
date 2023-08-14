bc81dc46e2d1525f2a372112d1e2fdbdffa53095
Edward Kuan <edward880814@gmail.com>
2023-08-14 11:49:13 +0800

    feat: remove post reaction from redis cache

--- src/shared/services/redis/reaction.cache.ts
+++ src/shared/services/redis/reaction.cache.ts
@@ -3,6 +3,8 @@ import Logger from 'bunyan';
 import { config } from '@root/config';
 import { ServerError } from '@global/helpers/error-handler';
 import { IReactionDocument, IReactions } from '@reaction/interfaces/reaction.interface';
+import { Helpers } from '@global/helpers/helpers';
+import { find } from 'lodash';
 
 const log: Logger = config.createLogger('reactionsCache');
 
@@ -24,7 +26,7 @@ export class ReactionCache extends BaseCache {
       }
 
       if (previousReaction) {
-        // call remove reaction method
+        this.removePostReactionFromCache(key, reaction.username, postReactions);
       }
       if (type) {
         await this.client.LPUSH(`reactions:${key}`, JSON.stringify(reaction));
@@ -37,5 +39,36 @@ export class ReactionCache extends BaseCache {
       log.error(error);
       throw new ServerError('Server error. Try again.');
     }
+  }
+
+  public async removePostReactionFromCache(key: string, username: string, postReactions: IReactions): Promise<void> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const response: string[] = await this.client.LRANGE(`reactions:${key}`, 0, -1);
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+      const userPreviousReaction: IReactionDocument = this.getPreviousReaction(response, username) as IReactionDocument;
+      multi.LREM(`reactions:${key}`, 1, JSON.stringify(userPreviousReaction));
+      await multi.exec();
+
+      const dataToSave: string[] = ['reactions', JSON.stringify(postReactions)];
+      for (let i = 0; i < dataToSave.length; i += 2) {
+        await this.client.HSET(`users:${key}`, dataToSave[i], dataToSave[i + 1]);
+      }
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
+
+  private getPreviousReaction(response: string[], username: string): IReactionDocument | undefined {
+    const list: IReactionDocument[] = [];
+    for (const item of response) {
+      list.push(Helpers.parseJson(item) as IReactionDocument);
+    }
+    return find(list, (listItem: IReactionDocument) => {
+      return listItem.username === username;
+    });
   }
 }
