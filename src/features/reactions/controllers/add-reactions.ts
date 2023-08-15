7f2924ef762f8c060ea6bd9284b1bc39d0bc6f51
Edward Kuan <edward880814@gmail.com>
2023-08-15 18:29:47 +0800

    feat: post reaction queues and worker && fix add reaction to mongodb issue

--- src/features/reactions/controllers/add-reactions.ts
+++ src/features/reactions/controllers/add-reactions.ts
@@ -3,8 +3,9 @@ import { ObjectId } from 'mongodb';
 import HTTP_STATUS from 'http-status-codes';
 import { joiValidation } from '@global/decorators/joi-validation.decorators';
 import { addReactionSchema } from '@reaction/schemes/reactions';
-import { IReactionDocument } from '@reaction/interfaces/reaction.interface';
+import { IReactionDocument, IReactionJob } from '@reaction/interfaces/reaction.interface';
 import { ReactionCache } from '@service/redis/reaction.cache';
+import { reactionQueue } from '@service/queues/reaction.queue';
 
 const reactionCache: ReactionCache = new ReactionCache();
 
@@ -23,6 +24,16 @@ export class Add {
 
     await reactionCache.savePostReactionToCache(postId, reactionObject, postReactions, type, previousReaction);
 
+    const databaseReactionData: IReactionJob = {
+      postId,
+      userTo,
+      userFrom: req.currentUser!.userId,
+      username: req.currentUser!.username,
+      type,
+      previousReaction,
+      reactionObject
+    };
+    reactionQueue.addReactionJob('addReactionToDB', databaseReactionData);
     res.status(HTTP_STATUS.OK).json({ message: 'Reaction added successfully' });
   }
 }
--- src/shared/services/db/reaction.service.ts
+++ src/shared/services/db/reaction.service.ts
@@ -4,15 +4,20 @@ import { IReactionDocument, IReactionJob } from '@reaction/interfaces/reaction.i
 import { ReactionModel } from '@reaction/models/reaction.schema';
 import { UserCache } from '@service/redis/user.cache';
 import { IUserDocument } from '@user/interfaces/user.interface';
+import { omit } from 'lodash';
 
 const userCache: UserCache = new UserCache();
 
 class ReactionService {
   public async addReactionDataToDB(reactionData: IReactionJob): Promise<void> {
     const { postId, userTo, userFrom, username, type, previousReaction, reactionObject } = reactionData;
+    let updatedReactionObject: IReactionDocument = reactionObject as IReactionDocument;
+    if (previousReaction) {
+      updatedReactionObject = omit(reactionObject, ['_id']);
+    }
     const updatedReaction: [IUserDocument, IReactionDocument, IPostDocument] = (await Promise.all([
       userCache.getUserFromCache(`${userTo}`),
-      ReactionModel.replaceOne({ postId, type: previousReaction, username }, reactionObject, { upsert: true }),
+      ReactionModel.replaceOne({ postId, type: previousReaction, username }, updatedReactionObject, { upsert: true }),
       PostModel.findOneAndUpdate(
         { _id: postId },
         {
@@ -23,9 +28,25 @@ class ReactionService {
         },
         { new: true }
       )
-    ])) as [IUserDocument, IReactionDocument, IPostDocument];
+    ])) as unknown as [IUserDocument, IReactionDocument, IPostDocument];
 
     // send reactions notification
+  }
+
+  public async removeReactionDataFromDB(reactionData: IReactionJob): Promise<void> {
+    const { postId, previousReaction, username } = reactionData;
+    await Promise.all([
+      ReactionModel.deleteOne({ postId, type: previousReaction, username }),
+      PostModel.updateOne(
+        { _id: postId },
+        {
+          $inc: {
+            [`reactions.${previousReaction}`]: -1
+          }
+        },
+        { new: true }
+      )
+    ]);
   }
 }
 export const reactionService: ReactionService = new ReactionService();
