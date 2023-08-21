c1b735a66fd09419d8c76f1d6cd3ccaba6afc9c2
Edward Kuan <edward880814@gmail.com>
2023-08-21 15:02:08 +0800

    feat: random users controller method

--- src/features/user/routes/userRoutes.ts
+++ src/features/user/routes/userRoutes.ts
@@ -14,6 +14,7 @@ class UserRoutes {
     this.router.get('/user/profile', authMiddleware.checkAuthentication, Get.prototype.profile);
     this.router.get('/user/profile/:userId', authMiddleware.checkAuthentication, Get.prototype.profileByUserId);
     this.router.get('/user/profile/posts/:username/:userId/:uId', authMiddleware.checkAuthentication, Get.prototype.profileAndPosts);
+    this.router.get('/user/profile/user/suggestions', authMiddleware.checkAuthentication, Get.prototype.randomUserSuggestions);
     return this.router;
   }
 }
