05b02f02504b650d28e6d9ad6738f9e514b0677d
Edward Kuan <edward880814@gmail.com>
2023-08-22 15:23:02 +0800

    feat: update basic info

--- src/features/user/routes/userRoutes.ts
+++ src/features/user/routes/userRoutes.ts
@@ -3,6 +3,7 @@ import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Get } from '@user/controllers/get-profile';
 import { Search } from '@user/controllers/search-user';
 import { Update } from '@user/controllers/change-password';
+import { Edit } from '@user/controllers/update-basic-info';
 
 class UserRoutes {
   private router: Router;
@@ -18,7 +19,11 @@ class UserRoutes {
     this.router.get('/user/profile/posts/:username/:userId/:uId', authMiddleware.checkAuthentication, Get.prototype.profileAndPosts);
     this.router.get('/user/profile/user/suggestions', authMiddleware.checkAuthentication, Get.prototype.randomUserSuggestions);
     this.router.get('/user/profile/search/:query', authMiddleware.checkAuthentication, Search.prototype.user);
+
     this.router.put('/user/profile/change-password', authMiddleware.checkAuthentication, Update.prototype.password);
+    this.router.put('/user/profile/basic-info', authMiddleware.checkAuthentication, Edit.prototype.info);
+    this.router.put('/user/profile/social-links', authMiddleware.checkAuthentication, Edit.prototype.social);
+
     return this.router;
   }
 }
