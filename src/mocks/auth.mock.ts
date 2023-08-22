877ce23acdd5b6b15ffab26e14feb3753562cceb
Edward Kuan <edward880814@gmail.com>
2023-08-22 15:46:57 +0800

    feat: add user controller unit test

--- src/mocks/auth.mock.ts
+++ src/mocks/auth.mock.ts
@@ -2,25 +2,21 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Response } from 'express';
 import { AuthPayload, IAuthDocument } from '@auth/interfaces/auth.interface';
-
 export const authMockRequest = (sessionData: IJWT, body: IAuthMock, currentUser?: AuthPayload | null, params?: any) => ({
   session: sessionData,
   body,
   params,
   currentUser
 });
-
 export const authMockResponse = (): Response => {
   const res: Response = {} as Response;
   res.status = jest.fn().mockReturnValue(res);
   res.json = jest.fn().mockReturnValue(res);
   return res;
 };
-
 export interface IJWT {
   jwt?: string;
 }
-
 export interface IAuthMock {
   _id?: string;
   username?: string;
@@ -30,7 +26,21 @@ export interface IAuthMock {
   avatarColor?: string;
   avatarImage?: string;
   createdAt?: Date | string;
+  currentPassword?: string;
+  newPassword?: string;
   confirmPassword?: string;
+  quote?: string;
+  work?: string;
+  school?: string;
+  location?: string;
+  facebook?: string;
+  instagram?: string;
+  twitter?: string;
+  youtube?: string;
+  messages?: boolean;
+  reactions?: boolean;
+  comments?: boolean;
+  follows?: boolean;
 }
 
 export const authUserPayload: AuthPayload = {
@@ -41,7 +51,6 @@ export const authUserPayload: AuthPayload = {
   avatarColor: '#9c27b0',
   iat: 12345
 };
-
 export const authMock = {
   _id: '60263f14648fed5246e322d3',
   uId: '1621613119252066',
@@ -49,32 +58,6 @@ export const authMock = {
   email: 'manny@me.com',
   avatarColor: '#9c27b0',
   createdAt: new Date(),
-  save: () => {}
+  save: () => {},
+  comparePassword: () => false
 } as unknown as IAuthDocument;
-
-export const signUpMockData = {
-  _id: '605727cd646eb50e668a4e13',
-  uId: '92241616324557172',
-  username: 'Manny',
-  email: 'manny@test.com',
-  avatarColor: '#ff9800',
-  password: 'manny1',
-  birthDay: { month: '', day: '' },
-  postCount: 0,
-  gender: '',
-  quotes: '',
-  about: '',
-  relationship: '',
-  blocked: [],
-  blockedBy: [],
-  bgImageVersion: '',
-  bgImageId: '',
-  work: [],
-  school: [],
-  placesLived: [],
-  createdAt: new Date(),
-  followersCount: 0,
-  followingCount: 0,
-  notifications: { messages: true, reactions: true, comments: true, follows: true },
-  profilePicture: 'https://res.cloudinary.com/ratingapp/image/upload/605727cd646eb50e668a4e13'
-};
