c1c7543880ee331b533e894bf15e1b899431df01
Edward Kuan <edward880814@gmail.com>
2023-10-04 00:35:29 +0800

    fix: change the nodejs installation method

--- deployment/userdata/user-data.sh
+++ deployment/userdata/user-data.sh
@@ -7,36 +7,18 @@ function program_is_installed {
   echo "$return_"
 }
 
-# Update the system and install required dependencies
-sudo yum update -y
-sudo yum install -y ca-certificates curl
-
-# Check if NodeJs is installed. If not, install it manually
-if [ $(program_is_installed node) == 0 ]; then
-  # Download and import the NodeSource GPG key
-  sudo mkdir -p /etc/pki/rpm-gpg
-  sudo curl -fsSL -o nodesource.gpg https://deb.nodesource.com/gpgkey/nodesource.gpg.key
-  sudo gpg --dearmor -o /etc/pki/rpm-gpg/nodesource.gpg < nodesource.gpg
-
-  # Set the desired Node.js version (change NODE_MAJOR as needed)
-  NODE_MAJOR=16
-
-  # Download the setup script
-  sudo curl -fsSL https://rpm.nodesource.com/setup_$NODE_MAJOR.x -o nodesource_setup.sh
-
-  # Run the setup script
-  sudo bash nodesource_setup.sh
-
-  # Install Node.js
-  sudo yum install -y nodejs
-fi
-
 sudo yum install ruby -y
 sudo yum install wget -y
 cd /home/ec2-user
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 sudo chmod +x ./install
 sudo ./install auto
+
+# Check if NodeJs is installed. If not, install it
+if [ $(program_is_installed node) == 0 ]; then
+  sudo yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+  sudo yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
+fi
 
 if [ $(program_is_installed git) == 0 ]; then
   sudo yum install git -y
