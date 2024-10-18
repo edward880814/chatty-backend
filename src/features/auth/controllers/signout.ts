0b5513dbe94f55766936b58f992c435cae9271b7
Edward Kuan <edward880814@gmail.com>
2024-10-18 23:24:32 +0800

    改回來

--- src/features/auth/controllers/signout.ts
+++ src/features/auth/controllers/signout.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 
 export class SignOut {
   public async update(req: Request, res: Response): Promise<void> {
+    req.session = null;
     res.status(HTTP_STATUS.OK).json({ message: 'Logout successful', user: {}, token: '' });
   }
 }
